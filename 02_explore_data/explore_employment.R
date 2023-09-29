58ab62c3a7a29998c71b39e659c977e9f456024b
daniellee70 <104069507+daniellee70@users.noreply.github.com>
2023-09-29 07:26:41 -0400

    Updated charts

--- 02_explore_data/explore_employment.R
+++ 02_explore_data/explore_employment.R
@@ -16,37 +16,7 @@ library(tidyquant)
 library(patchwork)
 
 # DATA -----
-
-# empl_Belknap      <- tidyquant::tq_get("LAUCN330010000000005", get = "economic.data", from = "1990-01-01")
-# empl_Carroll      <- tidyquant::tq_get("LAUCN330030000000005", get = "economic.data", from = "1990-01-01")
-# empl_Cheshire     <- tidyquant::tq_get("LAUCN330050000000005", get = "economic.data", from = "1990-01-01")
-# empl_Coos         <- tidyquant::tq_get("LAUCN330070000000005", get = "economic.data", from = "1990-01-01")
-# empl_Grafton      <- tidyquant::tq_get("LAUCN330090000000005", get = "economic.data", from = "1990-01-01")
-# empl_Hillsborough <- tidyquant::tq_get("LAUCN330110000000005", get = "economic.data", from = "1990-01-01")
-# empl_Merrimack    <- tidyquant::tq_get("LAUCN330130000000005", get = "economic.data", from = "1990-01-01")
-# empl_Rockingham   <- tidyquant::tq_get("LAUCN330150000000005", get = "economic.data", from = "1990-01-01")
-# empl_Strafford    <- tidyquant::tq_get("LAUCN330170000000005", get = "economic.data", from = "1990-01-01")
-# empl_Sullivan     <- tidyquant::tq_get("LAUCN330190000000005", get = "economic.data", from = "1990-01-01")
-# 
-# empl_all <- list(Belknap = empl_Belknap, 
-#                   Carroll = empl_Carroll,
-#                   Cheshire = empl_Cheshire,
-#                   Coos = empl_Coos,
-#                   Grafton = empl_Grafton,
-#                   Hillsborough = empl_Hillsborough,
-#                   Merrimack = empl_Merrimack,
-#                   Rockingham = empl_Rockingham,
-#                   Strafford = empl_Strafford, 
-#                   Sullivan = empl_Sullivan) %>% 
-#     enframe() %>% 
-#     unnest(value) %>% 
-#     select(county = name, date, empl = price)
-# 
-# empl_all
-# 
-# write_rds(empl_all, "00_data/empl_all.rds")
-
-empl_all <- read_rds("00_data/empl_all.rds")
+data_tbl <- read_rds("00_data/data_tbl.rds")
 
 data_tbl <- empl_all %>%
     select(county, date, empl) %>% 
